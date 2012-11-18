1039b0a575f8401421820c20fefe206a9633a932
Johannes Drever <johannes.drever@linova.de>
2012-11-18 12:24:48 +0100

    first commit

---
+++ WiggleIt/GCFinger.h
@@ -0,0 +1,13 @@
+//
+//  GCFinger.h
+//  WiggleIt
+//
+//  Created by Johannes Drever on 8/18/12.
+//  Copyright (c) 2012 Johannes Drever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCFinger : UIView
+
+@end
---
+++ WiggleIt/GCFinger.m
@@ -0,0 +1,31 @@
+//
+//  GCFinger.m
+//  WiggleIt
+//
+//  Created by Johannes Drever on 8/18/12.
+//  Copyright (c) 2012 Johannes Drever. All rights reserved.
+//
+
+#import "GCFinger.h"
+
+@implementation GCFinger
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
